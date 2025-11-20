ba675c3081d37a2be92392f1524855020889abde
Benjamin Alexander <benjamin.arctander@gmail.com>
2025-11-20 06:30:11 +0100

    Ensure we use the customPlayerController

--- Source/ZombieApocalypse/SimulationGameMode.cpp
+++ Source/ZombieApocalypse/SimulationGameMode.cpp
@@ -1,10 +1,11 @@
 // Copyright University of Inland Norway
 
 #include "SimulationGameMode.h"
+#include "CustomPlayerController.h"
 #include "SimulationHUD.h"
-
 
 ASimulationGameMode::ASimulationGameMode()
 {
 	HUDClass = ASimulationHUD::StaticClass();
+	PlayerControllerClass = ACustomPlayerController::StaticClass();
 }
