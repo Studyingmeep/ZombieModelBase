1fb3367c43872a348aeffaf7785e774f18301513
Benjamin Alexander <benjamin.arctander@gmail.com>
2025-11-20 05:24:01 +0100

    Updates Zombie class

---
+++ Source/ZombieApocalypse/Private/Zombie.cpp
@@ -0,0 +1,26 @@
+// Copyright University of Inland Norway
+
+#include "ZombieApocalypse/Public/Zombie.h"
+
+// Sets default values
+AZombie::AZombie()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AZombie::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AZombie::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
